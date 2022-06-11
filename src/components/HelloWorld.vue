d21bab4e80a69e51d9b62ea251700f7b590612df
ericstretton <eric.stretton@gmail.com>
2022-06-11 13:12:16 -0600

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="hello">
+    <input 
+    v-model="animalName"
+    type="text"  id="name">
+    <input
+    v-model="imageURL"
+    type="text"  id="image">
+
+
+    <button 
+    @click="postAnimal"
+    >POST</button>
+    <button 
+    @click="getAnimals"
+    >GET</button>
+
+    <div
+    v-for="animal in animalArr"
+    :key="animal.animalId"
+    >
+      {{animal.animalId}}
+      <h1>{{animal.animalName}}</h1>
+      <img :src="animal.imageURL">
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      animalName: null,
+      imageURL: null,
+      animalArr : []
+    }
+  },
+  methods: {
+    postAnimal() {
+      axios.request({
+        url : "http://127.0.0.1:5000/api/animals",
+        method : "POST",
+        data : {
+          animalName : this.animalName,
+          imageURL : this.imageURL
+        },
+      }).then(()=>{
+        this.getAnimals();
+      }).catch((error)=>{
+        console.log(error);
+      })
+      console.log(this.animalName, this.imageURL);
+    },
+    getAnimals(){
+      console.log("Running GET");
+      axios.request({
+        url : "http://127.0.0.1:5000/api/animals",
+        method : "GET"
+      }).then((response)=>{
+        console.log(response);
+        this.animalArr = response.data;
+      }).catch((error)=>{
+        console.log(error);
+      })
+    }
+    
+  },
+  mounted () {
+    this.getAnimals();
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+img{
+  width: 25vw;
+}
+div{
+  margin: 25px;
+}
+
+</style>
